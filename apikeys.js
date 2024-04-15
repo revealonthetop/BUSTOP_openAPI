585b725d117d605f54ee2dbbebf566d881d05c3b
revealonthetop <107919163+revealonthetop@users.noreply.github.com>
2024-04-15 22:41:27 +0900

    Add files via upload

---
+++ apikeys.js
@@ -0,0 +1,7 @@
+const config = {
+    // 기상청 단기예보 조회서비스
+    opneAPI_shotClimate: "hAdmwyPhz7cp45rb1yIPz45FUG6UgUfiKqkPUxdNaBQ1VB7rU8pF4MZxgEMkHCMxEWqNzKN6xyS9g7c%2Fh1AG6A%3D%3D",
+    opneAPI_longClimate: "hAdmwyPhz7cp45rb1yIPz45FUG6UgUfiKqkPUxdNaBQ1VB7rU8pF4MZxgEMkHCMxEWqNzKN6xyS9g7c%2Fh1AG6A%3D%3D"
+}
+
+export default config;
